07883d1251cb06173a8c489b1a5d90c8cb9b257e
filippo-falcone <falconefilippo98@hotmail.com>
2024-03-04 14:50:05 +0100

    feat: add userColor request

--- js/script.js
+++ js/script.js
@@ -4,5 +4,8 @@
 const userName = prompt('Inserisci il tuo nome');
 
 // Chiedi di inserire il cognome
-const userSurname = prompt('Inserisci ora il tuo Cognome')
-console.log(userSurname)
+const userSurname = prompt('Inserisci ora il tuo Cognome');
+
+// Chiedi di inserie il colore preferito
+const userColor = prompt('Inserisci adesso il tuo colore preferito');
+console.log(userColor);
